1632317ec0e0acd1e3d708bfe9130743759bf364
Yurith Rubio <iamyurith@gmail.com>
2024-02-20 15:27:32 +0100

    dist deleted

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  // window.location.href = "https://yurithrubio.myshopify.com/account/register"
 
   return (
     <>
